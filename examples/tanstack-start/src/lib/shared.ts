c7601f6bf18c50dc3f7713a992b9ed306a14708d
ctrlcat0x <sss.lata82@gmail.com>
2026-07-17 20:45:37 +0530

    Update example repository configuration

--- examples/tanstack-start/src/lib/shared.ts
+++ examples/tanstack-start/src/lib/shared.ts
@@ -4,7 +4,7 @@ export const docsImageRoute = '/og/docs';
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'watanuki',
+  user: 'ctrlcat0x',
+  repo: 'Watanuki',
   branch: 'main',
 };
